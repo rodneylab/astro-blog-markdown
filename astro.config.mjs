7bd17089b639c232bcc4d2d0037840badd06abb9
Rodney Lab <codesign@rodneylab.com>
2022-05-04 17:31:17 +0100

    feat: 🌟 add Turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
+import turbolinks from '@astrojs/turbolinks';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [svelte()],
+  integrations: [svelte(), turbolinks()],
 });
