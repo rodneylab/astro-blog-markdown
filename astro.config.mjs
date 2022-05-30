c63e1b1f5c52378df4af3bb79fd4061d69bbc256
Rodney Lab <codesign@rodneylab.com>
2022-05-30 05:05:51 +0100

    fix: 💫 remove deprecated turbolinks integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import svelte from '@astrojs/svelte';
-import turbolinks from '@astrojs/turbolinks';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [svelte(), turbolinks()],
+  integrations: [svelte()],
 });
