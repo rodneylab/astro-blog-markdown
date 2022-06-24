4ef194587bfecb4914a4da0ee7ee18f6c44e1908
Rodney Lab <codesign@rodneylab.com>
2022-06-24 08:36:09 +0100

    fix: 💫 add XML sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [svelte()],
+  integrations: [sitemap(), svelte()],
 });
