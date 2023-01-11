c14a636f3850e18fb957bdf5fef1a82a06566194
Rodney Lab <codesign@rodneylab.com>
2023-01-11 15:27:14 +0000

    fix: 💫 update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [sitemap(), svelte()],
+	integrations: [svelte()],
 });
