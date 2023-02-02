6a31ec61392bafe8232912607bd2510b620574d6
Rodney Lab <codesign@rodneylab.com>
2023-02-02 06:20:31 +0000

    fix: 💫 update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	preview: { port: 3001 },
+	server: { port: 3001 },
 	site: 'https://example.com',
 	integrations: [svelte()],
 });
