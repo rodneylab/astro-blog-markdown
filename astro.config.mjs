ab2dbdd0e9f98e303e7a1feac19460e3ddca9cf7
Rodney Lab <codesign@rodneylab.com>
2023-10-03 10:41:30 +0100

    fix(dependencies): 💫 update packages
    ┌─────────────────────────────┬─────────┬─────────┐
    │ Package                     │ From    │ To      │
    ├─────────────────────────────┼─────────┼─────────┤
    │ autoprefixer (dev)          │ 10.4.14 │ 10.4.16 │
    ├─────────────────────────────┼─────────┼─────────┤
    │ commitlint (dev)            │ 17.7.1  │ 17.7.2  │
    ├─────────────────────────────┼─────────┼─────────┤
    │ postcss (dev)               │ 8.4.27  │ 8.4.31  │
    ├─────────────────────────────┼─────────┼─────────┤
    │ prettier (dev)              │ 3.0.1   │ 3.0.3   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ stylelint (dev)             │ 15.10.2 │ 15.10.3 │
    ├─────────────────────────────┼─────────┼─────────┤
    │ svelte (dev)                │ 4.2.0   │ 4.2.1   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ svelte-check (dev)          │ 3.5.0   │ 3.5.2   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ typescript (dev)            │ 5.1.6   │ 5.2.2   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ @astrojs/rss (dev)          │ 2.4.4   │ 3.0.0   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ @astrojs/svelte (dev)       │ 3.1.0   │ 4.0.2   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ astro (dev)                 │ 2.10.7  │ 3.2.2   │
    ├─────────────────────────────┼─────────┼─────────┤
    │ prettier-plugin-astro (dev) │ 0.11.0  │ 0.12.0  │
    └─────────────────────────────┴─────────┴─────────┘

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	preview: { port: 3001 },
-	server: { port: 3001 },
 	site: 'https://example.com',
 	integrations: [svelte()],
 });
