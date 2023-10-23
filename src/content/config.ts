f38002d1f6dadf3e6b0256ecfa2682bd79dfb874
Rodney Lab <codesign@rodneylab.com>
2023-10-23 18:55:42 +0100

    fix: 💫 add banner images to posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,31 @@
+import { image_placeholder } from '@rodneylab/picpack';
 import { defineCollection, z } from 'astro:content';
+import { readFile } from 'node:fs/promises';
 
 const postsCollection = defineCollection({
-	schema: z.object({
-		postTitle: z.string(),
-		focusKeyphrase: z.string().optional(),
-		datePublished: z.string(),
-		lastUpdated: z.string(),
-		seoMetaDescription: z.string(),
-		featuredImage: z.string().optional(),
-		featuredImageAlt: z.string().optional(),
-		ogImage: z.string().optional(),
-		ogSquareImage: z.string().optional(),
-		twitterImage: z.string().optional(),
-		categories: z.string().array(),
-		tags: z.string().array(),
-		draft: z.boolean(),
-	}),
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			postTitle: z.string(),
+			focusKeyphrase: z.string().optional(),
+			datePublished: z.string(),
+			lastUpdated: z.string(),
+			seoMetaDescription: z.string(),
+			featuredImage: image(),
+			featuredImageAlt: z.string(),
+			ogImage: z.string().optional(),
+			ogSquareImage: z.string().optional(),
+			placeholder: z.string().transform(async (value) => {
+				const { buffer } = await readFile(`./src/content/posts/${value}`);
+				const imageBytes = new Uint8Array(buffer);
+				const { base64 } = image_placeholder(imageBytes);
+				return base64;
+			}),
+			twitterImage: z.string().optional(),
+			categories: z.string().array(),
+			tags: z.string().array(),
+			draft: z.boolean(),
+		}),
 });
 
 export const collections = {
