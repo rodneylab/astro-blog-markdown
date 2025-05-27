154e7a8481b5f0e277a373e9d2c50eeca64aeec6
Rodney Lab <codesign@rodneylab.com>
2025-05-27 18:19:40 +0100

    build(dependencies): 🧱 update packages
    ┌───────────────────────────────────────┬─────────┬─────────┐
    │ Package                               │ From    │ To      │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ @commitlint/config-conventional (dev) │ 19.8.0  │ 19.8.1  │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ commitlint (dev)                      │ 19.8.0  │ 19.8.1  │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ typescript (dev)                      │ 5.8.2   │ 5.8.3   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ @astrojs/svelte (dev)                 │ 7.0.6   │ 7.1.0   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ astro (dev)                           │ 5.5.2   │ 5.8.0   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ prettier-plugin-svelte (dev)          │ 3.3.3   │ 3.4.0   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ stylelint (dev)                       │ 16.16.0 │ 16.19.1 │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ svelte (dev)                          │ 5.23.1  │ 5.33.4  │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ svelte-check (dev)                    │ 4.1.5   │ 4.2.1   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ vite (dev)                            │ 6.2.2   │ 6.3.5   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ stylelint-config-recommended (dev)    │ 15.0.0  │ 16.0.0  │
    └───────────────────────────────────────┴─────────┴─────────┘

--- src/components/Icons/X.svelte
+++ src/components/Icons/X.svelte
@@ -1,6 +1,6 @@
 <script>
   let {
-    label = "Git Hub icon",
+    label = "X (social network) icon",
     colour = "inherit",
     ariaHidden = false,
     width = 24,
@@ -19,7 +19,7 @@
     viewBox="0 0 24 24"
     fill="none"
   >
-    <use href="/sprite.svg#github" />
+    <use href="/sprite.svg#x" />
   </svg>
 </div>
 
